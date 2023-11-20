580bed38b2170556a38cb7aec9c6096ffa894f34
IgorGonc <t.igorhus@gmail.com>
2023-11-20 13:19:48 -0300

    implementando nf

---
+++ src/main/java/models/Clientes.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tigor
+ */
+public class Clientes {
+    //Produto(1, "Lancôme",QUANTIDADE, 20.00);
+    ArrayList<Cliente> clientes;
+    public Clientes(){
+        this.clientes = new ArrayList<Cliente>();
+    }
+    
+    public void salvarCliente(Cliente cliente){
+        clientes.add(cliente);
+        System.out.println("cliente salvo");
+    }
+     
+   public void mostrarClientes(){
+       for (Cliente cliente : clientes) {
+           System.out.println("models.Clientes.mostrarClientes()");
+       }
+   }
+    
+}
